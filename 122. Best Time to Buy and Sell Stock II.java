193cb081397611be1fd27c866a62624bcf4b0170
yiqiesuiyuanba <zeyiliu@zeyis-mbp.campus.csudh.edu>
2019-02-26 16:17:57 -0800

    Stock II

---
+++ 122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * Time O(n)
+ * Space O(1)
+ *
+ */
+
+public class BestTimeToBuyAndSellStockII_122 {
+
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 2) {
+            return 0;
+        }
+
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+
+        }
+    }
+}
